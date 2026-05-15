247de7c0d945d727d4e7ebed5e2ef4472c96ffb4
sidstika-tech <sidstika@gmail.com>
2026-05-15 21:39:04 +0300

    Delete gemini.service.js

--- gemini.service.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const ctrl = require('../controllers/academy.controller');
-const { protect } = require('../middleware/auth.middleware');
-
-router.get('/daily', protect, ctrl.getDaily);
-router.post('/refresh', protect, ctrl.refresh);
-
-module.exports = router;
